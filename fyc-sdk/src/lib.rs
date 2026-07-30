1a97b237780aeecb247a1f5882f8ad981e96fef3
mroczect <mroczect@proton.me>
2026-07-31 01:33:29 +0700

    refactor(sdk): simplify duplicate error handling in seed_defaults

--- fyc-sdk/src/lib.rs
+++ fyc-sdk/src/lib.rs
@@ -20,12 +20,13 @@ pub fn seed_defaults(pool: &DbPool) -> Result<(), SdkError> {
     let perm_repo = PermissionRepo::new(pool.clone());
     let custom_repo = ProductCustomRepo::new(pool.clone());
 
-    if role_repo.get_role_by_name("admin")?.is_none() {
-        if let Err(DbError::DuplicateEntry(_)) = role_repo.create_role("admin", "Administrator") {}
+    if role_repo.get_role_by_name("admin")?.is_none()
+        && let Err(DbError::DuplicateEntry(_)) = role_repo.create_role("admin", "Administrator")
+    {
     }
-    if role_repo.get_role_by_name("kasir")?.is_none() {
-        if let Err(DbError::DuplicateEntry(_)) = role_repo.create_role("kasir", "Cashier") {}
-    }
+    if role_repo.get_role_by_name("kasir")?.is_none()
+        && let Err(DbError::DuplicateEntry(_)) = role_repo.create_role("kasir", "Cashier")
+    {}
 
     let perms = vec![
         ("user:create", "Create new users"),
@@ -42,24 +43,23 @@ pub fn seed_defaults(pool: &DbPool) -> Result<(), SdkError> {
     ];
 
     for (name, desc) in &perms {
-        if perm_repo.get_by_name(name)?.is_none() {
-            if let Err(DbError::DuplicateEntry(_)) = perm_repo.create(name, desc) {}
-        }
+        if perm_repo.get_by_name(name)?.is_none()
+            && let Err(DbError::DuplicateEntry(_)) = perm_repo.create(name, desc)
+        {}
     }
 
     if let Some(admin_role) = role_repo.get_role_by_name("admin")? {
         for (name, _) in &perms {
-            if let Some(perm) = perm_repo.get_by_name(name)? {
-                if let Err(DbError::DuplicateEntry(_)) =
+            if let Some(perm) = perm_repo.get_by_name(name)?
+                && let Err(DbError::DuplicateEntry(_)) =
                     role_repo.assign_permission_to_role(admin_role.id, perm.id)
-                {}
-            }
+            {}
         }
     }
 
-    if custom_repo.get_field_by_name("varian")?.is_none() {
-        if let Err(DbError::DuplicateEntry(_)) = custom_repo.create_field("varian", "text") {}
-    }
+    if custom_repo.get_field_by_name("varian")?.is_none()
+        && let Err(DbError::DuplicateEntry(_)) = custom_repo.create_field("varian", "text")
+    {}
 
     Ok(())
 }
