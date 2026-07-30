6ab5aded1c0d23365fe6dd17e3e31bef2ae462e8
mroczect <mroczect@proton.me>
2026-07-30 23:38:30 +0700

    feat(db): add audit and permission repos

--- fyc-db/src/repositories/mod.rs
+++ fyc-db/src/repositories/mod.rs
@@ -1,7 +1,11 @@
+pub mod audit_repo;
+pub mod permission_repo;
 pub mod role_repo;
 pub mod session_repo;
 pub mod user_repo;
 
+pub use audit_repo::AuditRepo;
+pub use permission_repo::PermissionRepo;
 pub use role_repo::RoleRepo;
 pub use session_repo::SessionRepo;
 pub use user_repo::UserRepo;
