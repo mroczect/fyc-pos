6df7b8e6742df30b2ca59dcd74ba3063b9283910
mroczect <mroczect@proton.me>
2026-07-30 21:26:09 +0700

    feat(db): add DbPool type alias

--- fyc-db/src/lib.rs
+++ fyc-db/src/lib.rs
@@ -6,3 +6,7 @@ pub mod repositories;
 pub use error::DbError;
 pub use models::*;
 pub use repositories::*;
+
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
+pub type DbPool = Pool<SqliteConnectionManager>;
