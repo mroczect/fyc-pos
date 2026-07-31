c1b9afca30b9e2ba660ccb3646d6d8aade21a02f
mroczect <mroczect@proton.me>
2026-07-31 18:51:24 +0700

    refactor(db): reorganize repository exports

--- fyc-db/src/repositories/mod.rs
+++ fyc-db/src/repositories/mod.rs
@@ -1,17 +1,11 @@
-pub mod audit_repo;
-pub mod order_repo;
-pub mod permission_repo;
-pub mod product_custom_repo;
-pub mod product_repo;
-pub mod role_repo;
-pub mod session_repo;
-pub mod user_repo;
+pub mod sqlite;
+pub mod traits;
 
-pub use audit_repo::AuditRepo;
-pub use order_repo::OrderRepo;
-pub use permission_repo::PermissionRepo;
-pub use product_custom_repo::ProductCustomRepo;
-pub use product_repo::ProductRepo;
-pub use role_repo::RoleRepo;
-pub use session_repo::SessionRepo;
-pub use user_repo::UserRepo;
+pub use sqlite::audit_repo::AuditRepo;
+pub use sqlite::order_repo::OrderRepo;
+pub use sqlite::permission_repo::PermissionRepo;
+pub use sqlite::product_custom_repo::ProductCustomRepo;
+pub use sqlite::product_repo::ProductRepo;
+pub use sqlite::role_repo::RoleRepo;
+pub use sqlite::session_repo::SessionRepo;
+pub use sqlite::user_repo::UserRepo;
