aa039ac6444a3f48dc31a26e80e6c0292c3cf136
mroczect <mroczect@proton.me>
2026-08-01 11:07:13 +0700

    feat(crypto): add lib.rs

--- lib_fyc_crypto/src/lib.rs
+++ lib_fyc_crypto/src/lib.rs
@@ -4,8 +4,8 @@ pub mod error;
 pub mod keygen;
 pub mod types;
 
-pub use decrypt::*;
-pub use encrypt::*;
+pub use decrypt::{decrypt_symmetric, decrypt_with_passphrase, decrypt_with_x25519};
+pub use encrypt::{encrypt_symmetric, encrypt_with_passphrase, encrypt_with_x25519};
 pub use error::CryptoError;
 pub use keygen::*;
 pub use types::*;
