a4cc35f6409bac975155a54441d28c7faafa580d
mroczect <mroczect@proton.me>
2026-08-05 01:23:24 +0700

    chore: remove lib_fyc_token/src/types.rs

--- lib_fyc_token/src/types.rs
+++
@@ -1,9 +0,0 @@
-use lib_fyc_role::types::Role;
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct TokenPayload {
-    pub user_id: String,
-    pub role: Role,
-    pub exp: u64,
-}
